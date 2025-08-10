3a8a5990edd048e059d4de4198c0955f1b443c64
Iosif Vieru <iosifvieru440@gmail.com>
2025-08-10 18:46:17 +0300

    HATEOAS support, better error handling and response codes

--- controllers/camera_controller.py
+++ controllers/camera_controller.py
@@ -2,23 +2,98 @@
 # 9 Aug. 2025
 
 from fastapi import (
-    APIRouter, HTTPException
+    APIRouter, HTTPException, Request
 )
 
 from persistance.schemas import CameraDataSchema
 from services.camera_service import (
-    insert_camera
+    insert_camera, get_data_from_all_cameras, get_camera_data_by_id
 )
 
 MAX_LENGTH = 64
+OBJECTID_LENGTH = 24
 router = APIRouter()
 
+def generate_hateoas_links(request: Request, resource_id: str):
+    links = {
+        "self": {
+            "href": str(request.url_for("get_camera_information", id=resource_id)),
+            "method": "GET"
+        },
+        "parent": {
+            "href": str(request.url_for("get_all_camera_data")),
+            "method": "GET"
+        },
+        "update": {
+            "href": str(request.url_for("get_camera_information", id=resource_id)),
+            "method": "PUT"
+        },
+        "delete": {
+            "href": str(request.url_for("get_camera_information", id=resource_id)),
+            "method": "DELETE"
+        }
+    }
+
+    return links
+
 @router.get('/api/cameras')
-def get_all_camera_data():
-    return "OK"
+async def get_all_camera_data(
+    request: Request,
+    city: str | None = None,
+    road: str | None = None
+    ):
+
+    params = {}
+
+    if city != None:
+        params["city"] = city
+
+    if road != None:
+        params["road"] = road
+
+    result = get_data_from_all_cameras(params)
+
+    if result is None:
+        raise HTTPException(status_code=503, detail="Failed to get the data.")
+
+    if not result:
+        raise HTTPException(status_code=404, detail="No camera data found.")
+
+    # adding HATEOAS link for every element
+    for r in result:
+        r["_links"] = generate_hateoas_links(request, r["_id"])
+
+    return {
+        "cameras": result,
+        "_links": "HATEOAS COMING SOON."
+    }
+
+@router.get('/api/cameras/{id}')
+async def get_camera_information(
+    request: Request, 
+    id: str
+    ):
+    
+    if(len(id) != OBJECTID_LENGTH):
+        raise HTTPException(status_code=422, detail="ID must be a 24-character hex string.")
+    
+    response = get_camera_data_by_id(id)
+
+    if response is None:
+        raise HTTPException(status_code=503, detail="Failed to get the data.")
+
+    if not response:
+        raise HTTPException(status_code=404, detail="No camera found with this id.")
+
+    links = generate_hateoas_links(request, id)
+
+    return {
+        "camera": dict(response),
+        "_links": links
+    }
 
 @router.post('/api/cameras')
-def insert_new_camera(data: CameraDataSchema):
+async def insert_new_camera(request: Request, data: CameraDataSchema):
     if len(data.city) == 0:
         raise HTTPException(status_code=422, detail="City length cannot be zero!")
 
@@ -37,9 +112,14 @@ def insert_new_camera(data: CameraDataSchema):
     if len(data.location) > MAX_LENGTH:
         raise HTTPException(status_code=422, detail=f"Location length cannot be greater than {MAX_LENGTH}!")
 
+    # inserting the data to persistance
     result = insert_camera(data)
+    if result is None:
+        raise HTTPException(status_code=503, detail=f"Failed to insert data.")
+
+    links = generate_hateoas_links(request, result["_id"])
 
     return {
         "camera": result,
-        "_links": "HATEOAS INCOMING."
+        "_links": links
     }
--- services/camera_service.py
+++ services/camera_service.py
@@ -3,17 +3,56 @@
 
 from persistance.schemas import CameraDataSchema
 from persistance.database import get_database
+from bson.objectid import ObjectId
+from bson.son import SON
 
 db = get_database('camera_data')
 collection = db.get_collection('cameras')
 
 def insert_camera(data: CameraDataSchema):
     data_as_dict = data.model_dump()
+    
+    if collection is None:
+        return None
 
     query_result = collection.insert_one(dict(data_as_dict))
 
     result = {
         "_id": str(query_result.inserted_id)
     } | data_as_dict
+
+    return result
+
+def get_data_from_all_cameras(params: dict):
+    response = []
+
+    if collection is None:
+        return None
+
+    cursor = None
+
+    if params is None:
+        cursor = collection.find({})
+    else:
+        cursor = collection.find(SON(params))
+
+    if cursor is None:
+        return None
+
+    for item in cursor:
+        camera_data = {
+            "_id": str(item["_id"]),
+        } | dict(CameraDataSchema(**item))
+
+        response.append(camera_data)
+
+    return response
+
+def get_camera_data_by_id(id: str):
+    if collection is None:
+        return None
+
+    result = collection.find_one({"_id": ObjectId(id)})
+    result["_id"] = str(result["_id"])
 
     return result
